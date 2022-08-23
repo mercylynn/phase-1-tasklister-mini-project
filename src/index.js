d49c3e03517706295a22805a17cd1f4c52a7f7da
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-23 06:33:23 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener('submit',(e) => 
+  {
+    e.preventDefault()
+    handleTodo(e.target.new_task.value)
+  })
+
+  function handleTodo(todo){
+    let li=document.createElement('li')
+    let btn=document.createElement('button')
+    btn.addEventListener('click',handleDelete)
+    btn.textContent= ' X '
+    li.textContent= ` ${todo} `
+    li.appendChild(btn)
+    //console.log(btn)
+    document.querySelector('#list').appendChild(li)
+  }
+
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
 });
+
+
+
+  
